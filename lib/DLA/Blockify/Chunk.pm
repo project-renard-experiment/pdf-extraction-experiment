a9cfc25fd67cd7336a8fd53c05e4f01d69f70130
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-11-18 16:51:25 -0600

    move Word package into its own module file

--- lib/DLA/Blockify/Chunk.pm
+++ lib/DLA/Blockify/Chunk.pm
@@ -10,22 +10,3 @@ has_xpath_object_list words => './words/wd' => 'DLA::Blockify::Word';
 finalize_class();
 
 1;
-
-package DLA::Blockify::Word;
-
-use strict;
-use warnings;
-
-use XML::Rabbit;
-
-has_xpath_value text => './@t';
-has_xpath_value width => './@w';
-has_xpath_value height => './@h';
-has_xpath_value x => './@x';
-has_xpath_value y => './@y';
-
-has_xpath_value _font_id => './@fid';
-
-finalize_class();
-
-1;
---
+++ lib/DLA/Blockify/Word.pm
@@ -0,0 +1,18 @@
+package DLA::Blockify::Word;
+
+use strict;
+use warnings;
+
+use XML::Rabbit;
+
+has_xpath_value text => './@t';
+has_xpath_value width => './@w';
+has_xpath_value height => './@h';
+has_xpath_value x => './@x';
+has_xpath_value y => './@y';
+
+has_xpath_value _font_id => './@fid';
+
+finalize_class();
+
+1;
